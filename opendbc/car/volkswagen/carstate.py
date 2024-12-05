50ef09e868c7427644493df76bd55b1d0d5fc6a5
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-12-05 18:14:57 +0100

    Update carstate.py add accels

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -280,7 +280,11 @@ class CarState(CarStateBase):
     ret.steeringRateDeg = pt_cp.vl["LWI_01"]["LWI_Lenkradw_Geschw"] * (1, -1)[int(pt_cp.vl["LWI_01"]["LWI_VZ_Lenkradw_Geschw"])]
     ret.steeringTorque = pt_cp.vl["LH_EPS_03"]["EPS_Lenkmoment"] * (1, -1)[int(pt_cp.vl["LH_EPS_03"]["EPS_VZ_Lenkmoment"])]
     ret.steeringPressed = abs(ret.steeringTorque) > self.CCP.STEER_DRIVER_ALLOWANCE
+    
     ret.yawRate = pt_cp.vl["MEB_ESP_04"]["Yaw_Rate"] * (1, -1)[int(pt_cp.vl["MEB_ESP_04"]["Yaw_Rate_Sign"])] * CV.DEG_TO_RAD
+    ret.latAccel = pt_cp.vl["MEB_ESP_04"]["Lateral_Accel"]
+    ret.longAccel = pt_cp.vl["MEB_ESP_04"]["Longitudinal_Accel"]
+    
     hca_status = self.CCP.hca_status_values.get(pt_cp.vl["MEB_EPS_01"]["LatCon_HCA_Status"])
     ret.steerFaultTemporary, ret.steerFaultPermanent = self.update_hca_state(hca_status)
 
