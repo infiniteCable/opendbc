f5317c03156519a24c686bf2f13f91572dff5cdb
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-01-31 20:30:12 +0100

    Update carstate.py

--- opendbc/car/volkswagen/carstate.py
+++ opendbc/car/volkswagen/carstate.py
@@ -366,11 +366,11 @@ class CarState(CarStateBase):
 
     # EV battery details
     ret.batteryDetails.charge = pt_cp.vl["Motor_16"]["MO_Energieinhalt_BMS"] # battery charge WattHours
-    ret.batteryDetails.voltage = 400 # battery output voltage
+    ret.batteryDetails.voltage = 355 # nominal voltage cupra born
     if self.CP.networkLocation == NetworkLocation.gateway:
-      ret.batteryDetails.heaterActive = main_cp.vl["MEB_HVEM_01"]["PTC_Status"] == 1 # battery heater active
-      ret.batteryDetails.soc = main_cp.vl["MEB_HVEM_01"]["Battery_SoC"] # battery SoC in percent
+      #ret.batteryDetails.heaterActive = main_cp.vl["MEB_HVEM_01"]["PTC_Status"] == 1 # battery heater active
       ret.batteryDetails.capacity = main_cp.vl["BMS_04"]["BMS_Kapazitaet_02"] * ret.batteryDetails.voltage # EV battery capacity WattHours
+      ret.batteryDetails.soc = ret.batteryDetails.charge / ret.batteryDetails.capacity * 100 if ret.batteryDetails.capacity > 0 else 0 # battery SoC in percent
       ret.batteryDetails.power = main_cp.vl["MEB_HVEM_01"]["Engine_Power"] # engine power output
       ret.batteryDetails.temperature = main_cp.vl["DCDC_03"]["DC_Temperatur"] # battery temperature
     
