fa5aa01a5faa71217f853140d915a800129260fc
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-02-01 16:45:42 +0100

    Update carcontroller.py remove testing parameter

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -265,7 +265,7 @@ class CarController(CarControllerBase):
                                                        CS.esp_hold_confirmation, CC.cruiseControl.override or CS.out.gasPressed)
         can_sends.append(self.CCS.create_acc_hud_control(self.packer_pt, CANBUS.pt, acc_hud_status, hud_control.setSpeed * CV.MS_TO_KPH,
                                                          hud_control.leadVisible, hud_control.leadDistanceBars + 1, show_distance_bars,
-                                                         CS.esp_hold_confirmation, distance, gap, fcw_alert, show_distance_bars))
+                                                         CS.esp_hold_confirmation, distance, gap, fcw_alert))
 
       else:
         lead_distance = 0
