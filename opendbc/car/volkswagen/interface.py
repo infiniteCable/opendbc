77e2c9bd5516cf458388d02742ff8b450e34ce8c
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-02-18 19:49:48 +0100

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -47,8 +47,8 @@ class CarInterface(CarInterfaceBase):
       if ret.networkLocation == NetworkLocation.gateway:
         ret.radarUnavailable = False
 
-      # TODO for anti EA intervention
-      #ret.flags |= VolkswagenFlags.STOCK_HCA_PRESENT.value # for MEB HCA_03 should be always present
+      # for anti EA intervention
+      ret.flags |= VolkswagenFlags.STOCK_HCA_PRESENT.value # for MEB HCA_03 should be always present
 
     else:
       # Set global MQB parameters
