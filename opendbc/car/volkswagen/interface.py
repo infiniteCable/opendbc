1a4e510b9d222e8ef56d2d6f8ce4211b129f7016
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-02-18 19:50:48 +0100

    Update interface.py

--- opendbc/car/volkswagen/interface.py
+++ opendbc/car/volkswagen/interface.py
@@ -95,7 +95,7 @@ class CarInterface(CarInterfaceBase):
 
     if ret.flags & VolkswagenFlags.MEB:
       ret.longitudinalActuatorDelay = 0.2
-      ret.radarDelay = 0.1 # testing
+      ret.radarDelay = 0.2 # testing
 
     ret.experimentalLongitudinalAvailable = ret.networkLocation == NetworkLocation.gateway or docs
     if experimental_long:
