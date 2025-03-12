4fa04c24c572b02a5e9c156071b3a4de3e506213
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-03-12 20:18:40 +0100

    Update safety_declarations.h curvature iso limit check declarations

--- opendbc/safety/safety_declarations.h
+++ opendbc/safety/safety_declarations.h
@@ -194,6 +194,7 @@ bool longitudinal_speed_checks(int desired_speed, const LongitudinalLimits limit
 bool longitudinal_gas_checks(int desired_gas, const LongitudinalLimits limits);
 bool longitudinal_transmission_rpm_checks(int desired_transmission_rpm, const LongitudinalLimits limits);
 bool longitudinal_brake_checks(int desired_brake, const LongitudinalLimits limits);
+bool curvature_iso_limit_check(int desired_curvature, bool steer_control_enabled, const AngleSteeringLimits limits);
 void pcm_cruise_check(bool cruise_engaged);
 
 void safety_tick(const safety_config *safety_config);
@@ -225,6 +226,10 @@ extern uint32_t ts_torque_check_last;
 extern uint32_t ts_steer_req_mismatch_last;  // last timestamp steer req was mismatched with torque
 
 extern bool lateral_only_mode;
+
+extern bool soft_limit_active;
+extern uint32_t soft_limit_timer;
+extern float soft_limit_start_curvature;
 
 // state for controls_allowed timeout logic
 extern bool heartbeat_engaged;             // openpilot enabled, passed in heartbeat USB command
