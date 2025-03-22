9f64578c7696cd8b711a4fd839b2c3541d0f746e
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-03-22 16:11:59 +0100

    Update carcontroller.py get this back in after successful test, car is happy with klr_01 and resumes from stop (>10Sec)

--- opendbc/car/volkswagen/carcontroller.py
+++ opendbc/car/volkswagen/carcontroller.py
@@ -232,6 +232,7 @@ class CarController(CarControllerBase):
 
       # Method 2: send capacitive steering wheel touched
       # propably EA could be stock activated only for cars equipped with capacitive steering wheel
+      # (also stock long does resume from stop as long as hands on is detected)
       if self.frame % 6 == 0:
         if self.CP.flags & VolkswagenFlags.STOCK_KLR_PRESENT:
           can_sends.append(mebcan.create_capacitive_wheel_touch(self.packer_pt, self.ext_bus, CC.enabled, CS.klr_stock_values))
@@ -328,11 +329,10 @@ class CarController(CarControllerBase):
                                                          lead_distance, hud_control.leadDistanceBars))
 
     # **** Stock ACC Button Controls **************************************** #
-    # DEACTIVATED BECAUSE OF capacitive steering wheel command testing, the car should resume from stop as long as hands on is detected
-    #gra_send_ready = self.CP.pcmCruise and CS.gra_stock_values["COUNTER"] != self.gra_acc_counter_last
-    #if gra_send_ready and (CC.cruiseControl.cancel or CC.cruiseControl.resume):
-    #  can_sends.append(self.CCS.create_acc_buttons_control(self.packer_pt, self.ext_bus, CS.gra_stock_values,
-    #                                                       cancel=CC.cruiseControl.cancel, resume=CC.cruiseControl.resume))
+    gra_send_ready = self.CP.pcmCruise and CS.gra_stock_values["COUNTER"] != self.gra_acc_counter_last
+    if gra_send_ready and (CC.cruiseControl.cancel or CC.cruiseControl.resume):
+      can_sends.append(self.CCS.create_acc_buttons_control(self.packer_pt, self.ext_bus, CS.gra_stock_values,
+                                                           cancel=CC.cruiseControl.cancel, resume=CC.cruiseControl.resume))
 
     new_actuators = actuators.as_builder()
     new_actuators.torque = self.apply_torque_last / self.CCP.STEER_MAX
