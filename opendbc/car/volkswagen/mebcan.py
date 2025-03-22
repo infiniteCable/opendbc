9449ecad2d47fc83ba225e8a6063486aaa85681d
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2025-03-22 17:32:17 +0100

    Update mebcan.py panda data message

--- opendbc/car/volkswagen/mebcan.py
+++ opendbc/car/volkswagen/mebcan.py
@@ -17,6 +17,13 @@ ACC_HUD_ACTIVE   = 3
 ACC_HUD_ENABLED  = 2
 ACC_HUD_DISABLED = 0
 
+
+def create_panda_data(packer, bus, roll))
+  values = {
+    "Roll": roll,
+  }
+  return packer.make_can_msg("PANDA_DATA_01", bus, values)
+
   
 def create_steering_control(packer, bus, apply_curvature, lkas_enabled, power, power_boost):
   values = {
